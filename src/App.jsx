78c4095ef2d8fcb09859704fe0c04086cf53e59c
王春雷🏂 <2294271895@qq.com>
2022-09-14 16:43:59 +0800

    init and react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      app
+    </div>
+  )
+}
+
+export default App
